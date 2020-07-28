514ac6f92091e13c3732e207f6793831e2a22ae5
Sharon Wang <wang.sharon93@gmail.com>
2020-07-28 02:30:33 -0400

    implement month time manipulations

--- order-by-date/order-by-date.py
+++ order-by-date/order-by-date.py
@@ -8,7 +8,10 @@ Attributes:
     ORDERED_SUMMARY_PATH (str): Path for time-sorted summary.
 
     NLP (NLP object): NLP object
-    MONTHS (list of str): Months.
+    MONTHS (list of list of int and str):
+        Months and their corresponding numbers.
+    WEEKDAYS (list of list of int and str):
+        Weekdays and their corresponding numbers.
 """
 
 import datetime
@@ -20,18 +23,43 @@ DATES_PATH = '../compress/out/dates/filtered_' + TOPIC + '_dates.txt'
 ORDERED_SUMMARY_PATH = 'out/ordered_' + TOPIC + '.txt'
 
 NLP = spacy.load('en_core_web_sm')
-MONTHS = [[1, "January"],
-          [2, "February"],
-          [3, "March"],
-          [4, "April"],
-          [5, "May"],
-          [6, "June"],
-          [7, "July"],
-          [8, "August"],
-          [9, "September"],
-          [10, "October"],
-          [11, "November"],
-          [12, "December"]]
+MONTHS = [[1, 'january'],
+          [1, 'jan'],
+          [2, 'february'],
+          [2, 'feb'],
+          [3, 'march'],
+          [3, 'mar'],
+          [4, 'april'],
+          [4, 'apr'],
+          [5, 'may'],
+          [6, 'june'],
+          [6, 'jun'],
+          [7, 'july'],
+          [7, 'jul'],
+          [8, 'august'],
+          [8, 'aug'],
+          [9, 'september'],
+          [9, 'sept'],
+          [10, 'october'],
+          [10, 'oct'],
+          [11, 'november'],
+          [11, 'nov'],
+          [12, 'december'],
+          [12, 'dec']]
+WEEKDAYS = [[1, 'monday'],
+            [1, 'mon'],
+            [2, 'tuesday'],
+            [2, 'tues'],
+            [3, 'wednesday'],
+            [3, 'wed'],
+            [4, 'thursday'],
+            [4, 'thurs'],
+            [5, 'friday'],
+            [5, 'fri'],
+            [6, 'saturday'],
+            [6, 'sat'],
+            [7, 'sunday'],
+            [7, 'sun']]
 
 def get_dates():
     """
@@ -67,26 +95,162 @@ def strip_symbols(s):
 def has_month(s):
     """
     Args:
-        s (str): Some string representing a spacy date.
+        s (str): Some string representing a date phrase.
 
     Returns:
         (list): List with the 0th element being a number representing the month,
-            (1st element).
+            (1st element) or an empty string if there is no month in `s`.
     """
     for month in MONTHS:
         if month[1].lower() in s:
             return month
     return ""
 
+def just_month(s):
+    """
+    Args:
+        s (str): Some string representing a date phrase.
+
+    Returns:
+        (bool): True if `s` is just a month.
+    """
+    s_split = [strip_symbols(word).lower() for word in s.split()]
+    if len(s_split) == 1:
+        return True
+    return False
+
+def previous_time(s):
+    """
+    Args:
+        s (str): Some string representing a date phrase.
+
+    Returns:
+        (bool): True if `s` is referring to a previous date.
+    """
+    s_split = [strip_symbols(word).lower() for word in s.split()]
+    if s_split[0] == "last":
+        return True
+    return False
+
+def future_month(s):
+    """
+    Args:
+        s (str): Some string representing a date phrase.
+
+    Returns:
+        (bool): True if `s` is referring to a future month.
+    """
+    s_split = [strip_symbols(word).lower() for word in s.split()]
+    if s_split[0] == "next":
+        return True
+    return False
+
+def mid_month(s):
+    """
+    Args:
+        s (str): Some string representing a data phrase.
+
+    Returns:
+        (bool): True if `s` is referring to the middle of a month.
+    """
+    if s[0:3].lower() == "mid":
+        return True
+    return False
+
+def change_to_previous_month(new_month_num, date):
+    """
+    Args:
+        new_month_num (int): Number corresponding to new month.
+        date (datetime obj): Publish date.
+
+    Returns:
+        (datetime obj): Real date of event in sentence.
+    """
+    publish_month_num = date.month
+    if new_month_num >= publish_month_num:
+        return date.replace(year=date.year - 1, month=new_month_num)
+    else:
+        return date.replace(month=new_month_num)
+
+def change_to_future_month(new_month_num, date):
+    """
+    Args:
+        new_month_num (int): Number corresponding to new month.
+        date (datetime obj): Publish date.
+
+    Returns:
+        (datetime obj): Real date of event in sentence.
+    """
+    publish_month_num = date.month
+    if new_month_num <= publish_month_num:
+        return date.replace(year=date.year + 1, month=new_month_num)
+    else:
+        return date.replace(month=new_month_num)
+
+def change_to_mid_month(date):
+    """
+    Args:
+        date (datetime obj): Current date associated with sentence.
+
+    Returns:
+        (datetime obj): Same as `date`, but with the day = 15.
+    """
+    return date.replace(day=15)
+
 def handle_month(ent, date):
-    month = has_month(str(ent))
-    if month:
-        publish_month = date.month
-        change_date = False
-        if month[0] != publish_month:
-            change_date = True
-        if change_date:
-            return date.replace(month=month[0])
+    """
+    Args:
+        ent (spacy date): A spacy entity representing a month.
+        date (datetime object): Datetime that may not agree with `ent`.
+
+    Returns:
+        (datetime object): Datetime fixed from `ent`.
+    """
+    new_month = has_month(str(ent))
+    if new_month:
+        publish_month_num = date.month
+        if just_month(str(ent)) or previous_time(str(ent)):
+            date = change_to_previous_month(new_month[0], date)
+        elif future_month(str(ent)):
+            date = change_to_future_month(new_month[0], date)
+
+        if mid_month(str(ent)):
+            date = change_to_mid_month(date)
+
+    return date
+
+def has_weekday(s):
+    """
+    Args:
+        s (str): Some string representing a spacy date.
+
+    Returns:
+        (list): List with the 0th element being a number representing the
+            weekday, (1st element) or an empty string if there is no weekday in
+            `s`.
+    """
+    for weekday in WEEKDAYS:
+        if weekday[1].lower() in s:
+            return weekday
+    return ""
+
+def change_to_previous_weekday(new_weekday_num, date):
+
+
+def handle_weekday(ent, date):
+    """
+    Args:
+        ent (spacy date): A spacy entity representing a weekday.
+        date (datetime object): Datetime that may not agree with `ent`.
+
+    Returns:
+        (datetime object): Datetime fixed from `ent`.
+    """
+    new_weekday = has_weekday(str(ent))
+    if new_weekday:
+        publish_weekday_num = date.isoweekday()
+        if previous_time(str(ent)):
+            date = change_to_previous_weekday(new_weekday[0], date)
     return date
 
 def fix(timestamped_sentences):
@@ -100,20 +264,23 @@ def fix(timestamped_sentences):
         (list of lists containing datetimes and str): List of lists where
         each inner list contains a sentence and its correct corresponding date.
     """
+    print(timestamped_sentences[0][1])
     for i, timestamped_sentence in enumerate(timestamped_sentences):
         date = timestamped_sentence[0]
         sentence = timestamped_sentence[1].lower()
         doc = NLP(sentence)
+
         for ent in doc.ents:
             if ent.label_ == "DATE":
                 timestamped_sentence[0] = handle_month(ent, date)
-                
+                timestamped_sentence[0] = handle_weekday(ent, timestamped_sentence[0])
 
+                print(0, sentence)
+                print(1, date)
+                print(2, ent)
+                print(3, timestamped_sentence[0])
 
-                #print(0, sentence)
-                #print(1, date)
-                #print(2, ent)
-                #print('\n')
+                print('\n')
 
     return timestamped_sentences
 
