28e967678a2c5fe9891cef81e9c2659fb33f0541
Sharon Wang <wang.sharon93@gmail.com>
2020-07-29 02:42:24 -0400

    handle days, weekdays, weeks, months, and years

--- order-by-date/order-by-date.py
+++ order-by-date/order-by-date.py
@@ -1,5 +1,5 @@
 """
-Orders summary sentences by time.
+Orders summary sentences by real time.
 
 Attributes:
     TOPIC (str): Topic of the summary.
@@ -16,6 +16,7 @@ Attributes:
 
 import datetime
 import spacy
+from word2number import w2n
 
 TOPIC = 'symptom'
 SUMMARY_PATH = '../compress/out/summaries/filtered_' + TOPIC + '.txt'
@@ -119,16 +120,18 @@ def just_month(s):
         return True
     return False
 
-def previous_time(s):
+def previous_month(s, new_month_num, date):
     """
     Args:
-        s (str): Some string representing a date phrase.
+        s (str): Some string representing a month phrase.
+        new_month_num (int): Number corresponding to new month.
+        date (datetime obj): Publish date.
 
     Returns:
-        (bool): True if `s` is referring to a previous date.
+        (bool): True if `s` is referring to a previous month.
     """
     s_split = [strip_symbols(word).lower() for word in s.split()]
-    if s_split[0] == "last":
+    if s_split[0] == "last" or new_month_num < date.month:
         return True
     return False
 
@@ -141,7 +144,7 @@ def future_month(s):
         (bool): True if `s` is referring to a future month.
     """
     s_split = [strip_symbols(word).lower() for word in s.split()]
-    if s_split[0] == "next":
+    if s_split[0] == "next" or new_month_num > date.month:
         return True
     return False
 
@@ -208,10 +211,9 @@ def handle_month(ent, date):
     """
     new_month = has_month(str(ent))
     if new_month:
-        publish_month_num = date.month
-        if just_month(str(ent)) or previous_time(str(ent)):
+        if previous_month(str(ent), new_month[0], date):
             date = change_to_previous_month(new_month[0], date)
-        elif future_month(str(ent)):
+        elif future_month(str(ent), new_month[0], date):
             date = change_to_future_month(new_month[0], date)
 
         if mid_month(str(ent)):
@@ -234,8 +236,71 @@ def has_weekday(s):
             return weekday
     return ""
 
-def change_to_previous_weekday(new_weekday_num, date):
+def previous_weekday(s, new_weekday_num, date):
+    """
+    Args:
+        s (str): Some string representing a weekday phrase.
+        new_weekday_num (int): Number corresponding to new weekday.
+        date (datetime obj): Publish date.
 
+    Returns:
+        (bool): True if `s` is referring to a previous weekday.
+        (int): Number of weeks ago that `s` is referring to.
+    """
+    s_split = [strip_symbols(word).lower() for word in s.split()]
+    if s_split[0] == "last":
+        return True, 1
+    elif len(s_split) == 3 and s_split[2] == "ago":
+        try:
+            num = w2n.word_to_num(s_split[0])
+            return True, num
+        except ValueError:
+            return False, 0
+    return False, 0
+
+def future_weekday(s, new_weekday_num, date):
+    """
+    Args:
+        s (str): Some string representing a weekday phrase.
+        new_weekday_num (int): Number corresponding to new weekday.
+        date (datetime obj): Publish date.
+
+    Returns:
+        (bool): True if `s` is referring to a future weekday.
+    """
+    s_split = [strip_symbols(word).lower() for word in s.split()]
+    if s_split[0] == "next":
+        return True
+    return False
+
+def change_to_previous_weekday(new_weekday_num, date, num):
+    """
+    Args:
+        new_weekday_num (int): Number corresponding to new weekday.
+        date (datetime obj): Publish date.
+        num (int): Number of weeks ago that `new_weekday_num` refers to.
+
+    Returns:
+        (datetime obj): Real date of event in sentence.
+    """
+    publish_weekday_num = date.isoweekday()
+    return date - datetime.timedelta(days=publish_weekday_num) + datetime.timedelta(days=new_weekday_num, weeks=-num)
+
+def change_to_future_weekday(new_weekday_num, date):
+    """
+    Args:
+        new_weekday_num (int): Number corresponding to new weekday.
+        date (datetime obj): Publish date.
+
+    Returns:
+        (datetime obj): Real date of event in sentence.
+    """
+    publish_weekday_num = date.isoweekday()
+    return date - datetime.timedelta(days=publish_weekday_num) + datetime.timedelta(days=new_weekday_num, weeks=1)
+
+def change_weekday(new_weekday_num, date):
+    publish_weekday_num = date.isoweekday()
+    return date - datetime.timedelta(days=publish_weekday_num) + datetime.timedelta(days=new_weekday_num)
 
 def handle_weekday(ent, date):
     """
@@ -248,9 +313,298 @@ def handle_weekday(ent, date):
     """
     new_weekday = has_weekday(str(ent))
     if new_weekday:
-        publish_weekday_num = date.isoweekday()
-        if previous_time(str(ent)):
-            date = change_to_previous_weekday(new_weekday[0], date)
+        if previous_weekday(str(ent), new_weekday[0], date)[0]:
+            date = change_to_previous_weekday(new_weekday[0], date, previous_weekday(str(ent), new_weekday[0], date)[1])
+        elif future_weekday(str(ent), new_weekday[0], date):
+            date = change_to_future_weekday(new_weekday[0], date)
+        else:
+            date = change_weekday(new_weekday[0], date)
+    return date
+
+def has_week(s):
+    """
+    Args:
+        s (str): Some string representing a spacy date.
+
+    Returns:
+        (bool): True if `s` is related to the concept of weeks.
+    """
+    if 'week' in s.lower() or 'weeks' in s.lower():
+        return True
+    return False
+
+def last_week(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a week
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents last week.
+    """
+    return len(split_phrase) == 2 and split_phrase[0] == "last" and split_phrase[1] == "week"
+
+def early_last_week(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a week
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents early last week.
+    """
+    return len(split_phrase) == 3 and split_phrase[0] == "early" and split_phrase[1] == "last" and split_phrase[2] == "week"
+
+def late_last_week(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a week
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents late last week.
+    """
+    return len(split_phrase) == 3 and split_phrase[0] == "late" and split_phrase[1] == "last" and split_phrase[2] == "week"
+
+def next_week(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a week
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents next week.
+    """
+    return len(split_phrase) == 2 and split_phrase[0] == "next" and split_phrase[1] == "week"
+
+def early_next_week(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a week
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents early next week.
+    """
+    return len(split_phrase) == 3 and split_phrase[0] == "early" and split_phrase[1] == "next" and split_phrase[2] == "week"
+
+def late_next_week(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a week
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents late next week.
+    """
+    return len(split_phrase) == 3 and split_phrase[0] == "late" and split_phrase[1] == "next" and split_phrase[2] == "week"
+
+def x_weeks_ago(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a week
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents x weeks ago.
+    """
+    return len(split_phrase) == 3 and split_phrase[1] == "weeks" and split_phrase[2] == "ago"
+
+def change_to_different_week(s, date):
+    """
+    Args:
+        s (str): Some string representing a week phrase.
+        date (datetime object): Publish date.
+
+    Returns:
+        (datetime object): Adjusted `date` based on `s`.
+    """
+    s_split = [strip_symbols(word).lower() for word in s.split()]
+    publish_weekday_num = date.isoweekday()
+    if last_week(s_split):
+        return date + datetime.timedelta(weeks=-1)
+    elif early_last_week(s_split):
+        return date - datetime.timedelta(days=publish_weekday_num) + datetime.timedelta(days=1, weeks=-1)
+    elif late_last_week(s_split):
+        return date - datetime.timedelta(days=publish_weekday_num) + datetime.timedelta(days=5, weeks=-1)
+    elif next_week(s_split):
+        return date + datetime.timedelta(weeks=1)
+    elif early_next_week(s_split):
+        return date - datetime.timedelta(days=publish_weekday_num) + datetime.timedelta(days=1, weeks=1)
+    elif late_next_week(s_split):
+        return date - datetime.timedelta(days=publish_weekday_num) + datetime.timedelta(days=5, weeks=1)
+    elif x_weeks_ago(s_split):
+        try:
+            num = w2n.word_to_num(s_split[0])
+            return date + datetime.timedelta(weeks=-num)
+        except ValueError:
+            return date
+    return date
+
+def handle_week(ent, date):
+    """
+    Args:
+        ent (spacy date): A spacy entity representing a week.
+        date (datetime object): Datetime that may not agree with `ent`.
+
+    Returns:
+        (datetime object): Datetime fixed from `ent`.
+    """
+    new_week = has_week(str(ent))
+    if new_week:
+        date = change_to_different_week(str(ent), date)
+    return date
+
+def has_year(s):
+    """
+    Args:
+        s (str): Some string representing a spacy date.
+
+    Returns:
+        (bool): True if `s` is related to the concept of year.
+    """
+    if 'year' in s.lower() or 'years' in s.lower():
+        return True
+    return False
+
+def last_year(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a year
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents last year.
+    """
+    return len(split_phrase) == 2 and split_phrase[0] == "last" and split_phrase[1] == "year"
+
+def next_year(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a year
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents last year.
+    """
+    return len(split_phrase) == 2 and split_phrase[0] == "next" and split_phrase[1] == "year"
+
+def x_years_ago(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a year
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents x years ago.
+    """
+    return len(split_phrase) == 3 and split_phrase[1] == "years" and split_phrase[2] == "ago"
+
+def change_to_different_year(s, date):
+    """
+    Args:
+        s (str): Some string representing a year phrase.
+        date (datetime object): Publish date.
+
+    Returns:
+        (datetime object): Adjusted `date` based on `s`.
+    """
+    s_split = [strip_symbols(word).lower() for word in s.split()]
+    if last_year(s_split):
+        return date.replace(year=date.year - 1)
+    elif next_year(s_split):
+        return date.replace(year=date.year + 1)
+    elif x_years_ago(s_split):
+        try:
+            num = w2n.word_to_num(s_split[0])
+            return date.replace(year=date.year - num)
+        except ValueError:
+            return date
+    return date
+
+def handle_year(ent, date):
+    """
+    Args:
+        ent (spacy date): A spacy entity representing a year.
+        date (datetime object): Datetime that may not agree with `ent`.
+
+    Returns:
+        (datetime object): Datetime fixed from `ent`.
+    """
+    new_year = has_year(str(ent))
+    if new_year:
+        date = change_to_different_year(str(ent), date)
+    return date
+
+def yesterday(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a day
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents yesterday.
+    """
+    return len(split_phrase) == 1 and split_phrase[0] == 'yesterday'
+
+def tomorrow(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a day
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents tomorrow.
+    """
+    return len(split_phrase) == 1 and split_phrase[0] == 'tomorrow'
+
+def x_days_ago(split_phrase):
+    """
+    Args:
+        split_phrase (list of str): List of words that representing a day
+            phrase.
+
+    Returns:
+        (bool): True if `split_phrase` represents x days ago.
+    """
+    return len(split_phrase) == 3 and split_phrase[1] == "days" and split_phrase[2] == "ago"
+
+def handle_day(ent, date):
+    """
+    Args:
+        ent (spacy date): A spacy entity representing a day.
+        date (datetime object): Datetime that may not agree with `ent`.
+
+    Returns:
+        (datetime object): Datetime fixed from `ent`.
+    """
+    ent_split = [strip_symbols(word).lower() for word in str(ent).split()]
+    if yesterday(ent_split):
+        return date + datetime.timedelta(days=-1)
+    elif tomorrow(ent_split):
+        return date + datetime.timedelta(days=1)
+    elif x_days_ago(ent_split):
+        try:
+            num = w2n.word_to_num(s_split[0])
+            return date + datetime.timedelta(days=-num)
+        except ValueError:
+            return date
+    return date
+
+def handle_relative_time_phrases(ent, date):
+    """
+    Args:
+        ent (spacy date): A spacy entity representing a date.
+        date (datetime object): Datetime that may not agree with `ent`.
+
+    Returns:
+        (datetime object): Datetime fixed from `ent`.
+    """
+    date = handle_day(ent, date)
+    date = handle_weekday(ent, date)
+    date = handle_week(ent, date)
+    date = handle_month(ent, date)
+    date = handle_year(ent, date)
     return date
 
 def fix(timestamped_sentences):
@@ -264,7 +618,6 @@ def fix(timestamped_sentences):
         (list of lists containing datetimes and str): List of lists where
         each inner list contains a sentence and its correct corresponding date.
     """
-    print(timestamped_sentences[0][1])
     for i, timestamped_sentence in enumerate(timestamped_sentences):
         date = timestamped_sentence[0]
         sentence = timestamped_sentence[1].lower()
@@ -272,8 +625,7 @@ def fix(timestamped_sentences):
 
         for ent in doc.ents:
             if ent.label_ == "DATE":
-                timestamped_sentence[0] = handle_month(ent, date)
-                timestamped_sentence[0] = handle_weekday(ent, timestamped_sentence[0])
+                timestamped_sentence[0] = handle_relative_time_phrases(ent, date)
 
                 print(0, sentence)
                 print(1, date)
@@ -313,7 +665,5 @@ sentences = get_sentences()
 
 timestamped_sentences = get_timestamped_sentences(dates, sentences)
 timestamped_sentences.sort(key=lambda i: i[0])
-
-timestamped_sentences[1][0] = datetime.datetime(2020, 5, 17)
 
 output(timestamped_sentences)
